a0babae24ae4007d8d6c194095dc4d356cf7595c
jhflores95 <jorgehugo95@gmail.com>
2018-02-07 00:09:29 -0800

    Task 4 done

--- task2/lab3_inc.sv
+++ task2/lab3_inc.sv
@@ -41,14 +41,6 @@ parameter PURPLE = 3'b101;
 parameter YELLOW = 3'b110;
 parameter WHITE = 3'b111;
 
-// Pick your poison.
-
-parameter bg_color = BLACK;
-parameter puck_color = PURPLE;
-parameter paddle_color = YELLOW;
-parameter border_colour = WHITE;
-
-
 // We are going to write this as a state machine.  The following
 // is a list of states that the state machine can be in.
 
@@ -58,7 +50,8 @@ typedef enum int unsigned {INIT = 1 , START = 2,
               DRAW_LEFT_ENTER = 64, DRAW_LEFT_LOOP = 128, IDLE = 256, 
               ERASE_PADDLE_ENTER = 512, ERASE_PADDLE_LOOP = 1024, 
               DRAW_PADDLE_ENTER = 2048, DRAW_PADDLE_LOOP = 4096, 
-              ERASE_PUCK = 8192, DRAW_PUCK = 16384 } draw_state_type;  
+              ERASE_PUCK = 8192, ERASE_PUCK2 = 16384, 
+              DRAW_PUCK = 32768, DRAW_PUCK2 = 65536} draw_state_type;  
 
 // Here are some parameters that we will use in the code. 
  
@@ -73,22 +66,28 @@ parameter TOP_LINE = 4;
 parameter RIGHT_LINE = SCREEN_WIDTH - 5;
 parameter LEFT_LINE = 5;
 
-// These parameters describe the starting location for the puck 
-parameter FACEOFF_X = SCREEN_WIDTH / 2;
+// These parameters describe the starting location for the puck(s)
+parameter FACEOFF_X = SCREEN_WIDTH / 2 + 8;
 parameter FACEOFF_Y = SCREEN_HEIGHT / 2;
+parameter FACEOFF_X_2 = SCREEN_WIDTH / 2 - 8;
   
 // Starting Velocity
-// Ball moves to the top right (45 deg to horizontal)
+
+// Ball moves top right (45 deg to horizontal)
 parameter VELOCITY_START_X = 1;
 parameter VELOCITY_START_Y = -1;
+
+// Puck moves bottom right(-45 deg to horizontal)
+parameter VELOCITY_START_X_2 = 1;
+parameter VELOCITY_START_Y_2 = 1;
   
 // This parameter indicates how many times the counter should count in the
 // START state between each invocation of the main loop of the program.
 // A larger value will result in a slower game.  The current setting will    
-// cause the machine to wait in the start state for 1/8 of a second between 
+// cause the machine to wait in the start state for 1/6 of a second between 
 // each invocation of the main loop.  The 50000000 is because we are
 // clocking our circuit with a 50Mhz clock. 
   
-parameter LOOP_SPEED = 50000000/8;  // 8Hz
+parameter LOOP_SPEED = 50000000/6;  // 6Hz
   
 `endif // _my_incl_vh_
